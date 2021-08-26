32ba16e13e686cc54fac2c6770897a5f373c362a
helixanon <helixanon@gmail.com>
2021-08-26 10:42:31 +0300

    boilerplate #2

--- Rotations.lua
+++ Rotations.lua
@@ -1,46 +1,15 @@
---[[local LAM = LibAddonMenu2
-local panelName = "RotationsSettingsPanel"
- 
-local panelData = {
-    type = "panel",
-    name = "Rotations",
-    author = "@helixanon",
-}
-local optionsData = {
-    {
-        type = "checkbox",
-        name = "Skip dialogues",
-        getFunc = function() return QuestSkipper.SavedVariables.SkipDialogues end,
-        setFunc = function(value) QuestSkipper.SavedVariables.SkipDialogues = value end
-    },
-    {
-        type = "checkbox",
-        name = "Skip stable training",
-        getFunc = function() return QuestSkipper.SavedVariables.SkipStableTraining end,
-        setFunc = function(value) QuestSkipper.SavedVariables.SkipStableTraining = value end
-    },
-    {
-      type        = "editbox",
-      name        = "Riding skills order",
-      getFunc     = function() return QuestSkipper.SavedVariables.HorseSkillsOrder end,
-      setFunc     = function(value) QuestSkipper.SavedVariables.HorseSkillsOrder = value end,
-      isMultiline = true,
-      textType    = TEXT_TYPE_ALL,
-      width       = "full"
-    }
-}]]--
-
-Rotations = {}
+Rotations = Rotations or {}
 Rotations.name = "Rotations"
+
+function Rotations.OnUpdate(self, time)
+
+end
 
 function Rotations:Initialize()
     EVENT_MANAGER:UnregisterForEvent(self.name, EVENT_ADD_ON_LOADED)
-    
-    --[[LAM:RegisterAddonPanel(panelName, panelData)
-    LAM:RegisterOptionControls(panelName, optionsData)]]--
 end
 
-function Rotations.OnAddOnLoaded(event, addonName)
+function Rotations.OnAddOnLoaded(_, addonName)
     if addonName == Rotations.name then
         Rotations:Initialize()
     end
