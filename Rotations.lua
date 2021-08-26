9fdd7fa1f1193dab8c9bf3d062870c11e00c0005
helixanon <helixanon@gmail.com>
2021-08-26 18:44:40 +0300

    good chunk of code

--- Rotations.lua
+++ Rotations.lua
@@ -1,11 +1,54 @@
 Rotations = Rotations or {}
 Rotations.name = "Rotations"
+Rotations.Timers = {}
+
+Rotations.AdditionalSkillDurations = {
+    --	concealed weapon
+    [25267] = 0,
+
+    --	barbed trap (reapply every 16 sec due to 2 sec arming)
+    [40382] = 16,
+
+    --siphoning attacks
+    --[36935] = 55,
+}
 
 function Rotations.OnUpdate(self, time)
+	--todo rework
+	for i = 3, 7 do
+		local abilityId = GetSlotBoundId(i)
+		if Rotations.Timers[abilityId] == nil then
+			Rotations.Timers[abilityId] = 0
+		end
+	end
+	local abilityToCastNext = -1
+	for k, v in pairs(Rotations.Target) do
+		if Rotations.Timers[v] ~= nil and Rotations.Timers[v] < time then
+			abilityToCastNext = v
+			break
+		end
+	end
+	RotationsQueueControlAlert:SetTexture(GetAbilityIcon(abilityToCastNext))
+end
 
+function Rotations.GetCorrectSkillSlot(slotNum)
+    return (GetActiveWeaponPairInfo() == 2) and (slotNum + 5) or slotNum
+end
+
+function Rotations.OnActionSlotAbilityUsed(eventCode, slotNum)
+    if (slotNum == 1) or (slotNum == 2) then
+        return
+    end
+    local abilityId = GetSlotBoundId(slotNum)
+    local duration = GetAbilityDuration(abilityId) / 1000
+    if Rotations.AdditionalSkillDurations[abilityId] ~= nil then
+        duration = Rotations.AdditionalSkillDurations[abilityId]
+    end
+    Rotations.Timers[abilityId] = (GetGameTimeMilliseconds() / 1000) + duration
 end
 
 function Rotations:Initialize()
+    EVENT_MANAGER:RegisterForEvent(self.name, EVENT_ACTION_SLOT_ABILITY_USED, self.OnActionSlotAbilityUsed)
     EVENT_MANAGER:UnregisterForEvent(self.name, EVENT_ADD_ON_LOADED)
 end
 
---
+++ priorityLists/default.lua
@@ -0,0 +1,22 @@
+Rotations = Rotations or {}
+
+Rotations.Target = {
+	61919,			--	Nightblade	- 	Assassination		-	Merciless Resolve
+	61930,			--	Nightblade	-	Assassination		-	Merciless Resolve Proc
+	39053,			--	Weapon		-	Destruction Staff	-	Unstable Wall of Fire
+	36049,			--	Nightblade	-	Shadow				-	Twisting Path 
+	35434,			--	Nightblade	-	Shadow				-	Dark Shade
+	40382,			--	Guild		-	Fighters' Guild		-	Barbed trap
+	36943,			--	Nightblade	-	Siphoning			-	Debilitate
+	40457,			--	Guild		-	Mages Guild			-	Degeneration
+	36935			--	Nightblade	-	Siphoning			-	Siphoning Attacks
+}
+
+Rotations.Spammables = {
+	25267,			--	Nightblade	-	Shadow				-	Concealed Weapon
+	34851			--	Nightblade	-	Assassination		-	Impale
+}
+
+Rotations.SkillsToDropAtHpPercent = {
+	{25267, 0.25}	--	Nightblade	-	Shadow				-	Concealed Weapon
+}
