dabb7043b8a73b27241f9d83cb498f2881d2e9b6
helixanon <helixanon@gmail.com>
2021-08-30 01:30:39 +0300

    beautify

--- priorityLists/default.lua
+++ priorityLists/default.lua
@@ -1,69 +1,63 @@
 Rotations = Rotations or {}
 
 Rotations.Dots = {
-	61919,				--	Nightblade		- 	Assassination		-	Merciless Resolve
-	61930,				--	Nightblade		-	Assassination		-	Merciless Resolve Proc
-	
-	39053,				--	Weapon			-	Destruction Staff	-	Unstable Wall of Fire
-	--39052,			--	Weapon			-	Destruction Staff	-	Unstable Wall of Elements
+    61919, --	Nightblade		- 	Assassination		-	Merciless Resolve
+    61930, --	Nightblade		-	Assassination		-	Merciless Resolve Proc
+    39053, --	Weapon			-	Destruction Staff	-	Unstable Wall of Fire
+    --39052,			--	Weapon			-	Destruction Staff	-	Unstable Wall of Elements
 
-	20930,				--	Dragonknight	-	Ardent Flame		-	Engulfing Flames
-	35823,				--	Dragonknight	-	Ardent Flame		-	Flames of Oblivion
-	20660,				--	Dragonknight	-	Ardent Flame		-	Burning Embers
-	22095,				--	Templar			-	Dawn's Wrath		-	Solar Barrage
-	26869,				--	Templar			-	Aedric Spear		-	Blazing Spear
-	36049,				--	Nightblade		-	Shadow				-	Twisting Path
-	24328,				--	Sorcerer		-	Daedric Summoning	-	Daedric Prey
-	114716,				--	Sorcerer		-	Dark Magic			-	Crystal Shards Proc
-	42028,				--	Guild			-	Undaunted			-	Mystic Orb
-	35434,				--	Nightblade		-	Shadow				-	Dark Shade
-	40382,				--	Guild			-	Fighters' Guild		-	Barbed trap
-	103706,				--	Guild			-	Psijic Order		-	Channeled Acceleration
-	32710,				--	Dragonknight	-	Earthen Heart		-	Eruption
-	40465,				--	Guild			-	Mages Guild			-	Scalding Rune
-	36943,				--	Nightblade		-	Siphoning			-	Debilitate
-	77182,				--	Sorcerer		-	Daedric Summoning	-	Summon Volatile Familiar
-	40457,				--	Guild			-	Mages Guild			-	Degeneration
-	22259,				--	Templar			-	Restoring Light		-	Ritual of Retribution
-	
-	36935,				--	Nightblade		-	Siphoning			-	Siphoning Attacks
-	22240,				--	Templar			-	Restoring Light		-	Channeled Focus
-
-	21765				--	Templar			-	Dawn's Wrath		-	Purifying Light 			(low priority to make sure that at least 1 spammable gets casted after this)
+    20930, --	Dragonknight	-	Ardent Flame		-	Engulfing Flames
+    35823, --	Dragonknight	-	Ardent Flame		-	Flames of Oblivion
+    20660, --	Dragonknight	-	Ardent Flame		-	Burning Embers
+    22095, --	Templar			-	Dawn's Wrath		-	Solar Barrage
+    26869, --	Templar			-	Aedric Spear		-	Blazing Spear
+    36049, --	Nightblade		-	Shadow				-	Twisting Path
+    24328, --	Sorcerer		-	Daedric Summoning	-	Daedric Prey
+    114716, --	Sorcerer		-	Dark Magic			-	Crystal Shards Proc
+    42028, --	Guild			-	Undaunted			-	Mystic Orb
+    35434, --	Nightblade		-	Shadow				-	Dark Shade
+    40382, --	Guild			-	Fighters' Guild		-	Barbed trap
+    103706, --	Guild			-	Psijic Order		-	Channeled Acceleration
+    32710, --	Dragonknight	-	Earthen Heart		-	Eruption
+    40465, --	Guild			-	Mages Guild			-	Scalding Rune
+    36943, --	Nightblade		-	Siphoning			-	Debilitate
+    77182, --	Sorcerer		-	Daedric Summoning	-	Summon Volatile Familiar
+    40457, --	Guild			-	Mages Guild			-	Degeneration
+    22259, --	Templar			-	Restoring Light		-	Ritual of Retribution
+    36935, --	Nightblade		-	Siphoning			-	Siphoning Attacks
+    22240, --	Templar			-	Restoring Light		-	Channeled Focus
+    21765 --	Templar			-	Dawn's Wrath		-	Purifying Light 			(low priority to make sure that at least 1 spammable gets casted after this)
 }
 
 Rotations.Spammables = {
-	25267,				--	Nightblade		-	Shadow				-	Concealed Weapon
-	34835,				--	Nightblade		-	Siphoning			-	Swallow Soul
-	34851,				--	Nightblade		-	Assassination		-	Impale
-	46324,				--	Sorcerer		-	Dark Magic			-	Crystal Shards
-	26797,				--	Templar			-	Aedric Spear		-	Puncturing Sweeps
-	63046,				--	Templar			-	Dawn's Wrath		-	Radiant Oppression
-	20805				--	Dragonknight	-	Ardent Flame		-	Molten Whip
+    25267, --	Nightblade		-	Shadow				-	Concealed Weapon
+    34835, --	Nightblade		-	Siphoning			-	Swallow Soul
+    34851, --	Nightblade		-	Assassination		-	Impale
+    46324, --	Sorcerer		-	Dark Magic			-	Crystal Shards
+    26797, --	Templar			-	Aedric Spear		-	Puncturing Sweeps
+    63046, --	Templar			-	Dawn's Wrath		-	Radiant Oppression
+    20805 --	Dragonknight	-	Ardent Flame		-	Molten Whip
 }
 
 Rotations.SkillsToDropAtHpPercent = {
-	[25267] = 0.25,		--	Nightblade		-	Shadow				-	Concealed Weapon
-	[34835] = 0.25,		--	Nightblade		-	Siphoning			-	Swallow Soul
-	[61919] = 0.25,		--	Nightblade		- 	Assassination		-	Merciless Resolve
-	[61930] = 0.25,		--	Nightblade		-	Assassination		-	Merciless Resolve Proc
-	[36943] = 0.25,		--	Nightblade		-	Siphoning			-	Debilitate
-	[40457] = 0.25,		--	Guild			-	Mages Guild			-	Degeneration
-	[36935] = 0.25,		--	Nightblade		-	Siphoning			-	Siphoning Attacks
-	[26797] = 0.25,		--	Templar			-	Aedric Spear		-	Puncturing Sweeps
-	
-	[22095] = 0.2,		--	Templar			-	Dawn's Wrath		-	Solar Barrage
-	
-	[42028] = 0.1,		--	Guild			-	Undaunted			-	Mystic Orb
-	
-	[26869] = 0.05,		--	Templar			-	Aedric Spear		-	Blazing Spear
-	[22240] = 0.05,		--	Templar			-	Restoring Light		-	Channeled Focus
-	[22259] = 0.05,		--	Templar			-	Restoring Light		-	Ritual of Retribution
-	[35434] = 0.05,		--	Nightblade		-	Shadow				-	Dark Shade
-	[40382] = 0.05,		--	Guild			-	Fighters' Guild		-	Barbed trap
-	[103706] = 0.05,	--	Guild			-	Psijic Order		-	Channeled Acceleration
-	[39053] = 0.05,		--	Weapon			-	Destruction Staff	-	Unstable Wall of Fire
-	[39052] = 0.05,		--	Weapon			-	Destruction Staff	-	Unstable Wall of Elements
-	[36049] = 0.05,		--	Nightblade		-	Shadow				-	Twisting Path
-	[21765] = 0.05		--	Templar			-	Dawn's Wrath		-	Purifying Light
+    [25267] = 0.25, --	Nightblade		-	Shadow				-	Concealed Weapon
+    [34835] = 0.25, --	Nightblade		-	Siphoning			-	Swallow Soul
+    [61919] = 0.25, --	Nightblade		- 	Assassination		-	Merciless Resolve
+    [61930] = 0.25, --	Nightblade		-	Assassination		-	Merciless Resolve Proc
+    [36943] = 0.25, --	Nightblade		-	Siphoning			-	Debilitate
+    [40457] = 0.25, --	Guild			-	Mages Guild			-	Degeneration
+    [36935] = 0.25, --	Nightblade		-	Siphoning			-	Siphoning Attacks
+    [26797] = 0.25, --	Templar			-	Aedric Spear		-	Puncturing Sweeps
+    [22095] = 0.2, --	Templar			-	Dawn's Wrath		-	Solar Barrage
+    [42028] = 0.1, --	Guild			-	Undaunted			-	Mystic Orb
+    [26869] = 0.05, --	Templar			-	Aedric Spear		-	Blazing Spear
+    [22240] = 0.05, --	Templar			-	Restoring Light		-	Channeled Focus
+    [22259] = 0.05, --	Templar			-	Restoring Light		-	Ritual of Retribution
+    [35434] = 0.05, --	Nightblade		-	Shadow				-	Dark Shade
+    [40382] = 0.05, --	Guild			-	Fighters' Guild		-	Barbed trap
+    [103706] = 0.05, --	Guild			-	Psijic Order		-	Channeled Acceleration
+    [39053] = 0.05, --	Weapon			-	Destruction Staff	-	Unstable Wall of Fire
+    [39052] = 0.05, --	Weapon			-	Destruction Staff	-	Unstable Wall of Elements
+    [36049] = 0.05, --	Nightblade		-	Shadow				-	Twisting Path
+    [21765] = 0.05 --	Templar			-	Dawn's Wrath		-	Purifying Light
 }
